fe2b5ce9cbed6fe60d894d7f9ae9f12277c6b65c
Arnaud Oisel <arnaud.oisel@gmail.com>
2015-03-12 20:20:01 +0100

    Add group class

---
+++ src/main/java/fr/pda/bean/Group.java
@@ -0,0 +1,35 @@
+package fr.pda.bean;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Arnaud on 12/03/15.
+ */
+public class Group {
+
+    public int id;
+
+    private Set<Serveur> serveurs = new HashSet<Serveur>();
+
+    private int[] capacites ;
+
+    public Group(int nbRangees) {
+        capacites = new int[nbRangees];
+    }
+
+    public void add(Serveur serveur) {
+        serveurs.add(serveur);
+    }
+
+    public static Group getGroupFaible(List<Group> groups) {
+        return null;
+    }
+
+    public int getSommeCapaciteSaufPlusForte() {
+        return -1;
+    }
+
+
+}
