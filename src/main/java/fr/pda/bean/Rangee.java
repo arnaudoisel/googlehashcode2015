91c89097acb5b035adaf678da9f754be34e00dc9
unknown <BRICONO@LFR001625.corp.capgemini.com>
2015-03-12 20:05:55 +0100

    aezz

--- src/main/java/fr/pda/bean/Rangee.java
+++ src/main/java/fr/pda/bean/Rangee.java
@@ -13,7 +13,24 @@ public class Rangee {
 	}
 	
 	public int addServeur(Serveur serveur){
-		return 0;
+		int taille = serveur.nbEmplacement;
+		for (int i=0; i<tUse.length; i++){
+			if(tUse[i]==null){
+				boolean ok = true;
+				for (int j = i; j < i+taille; j++) {
+					if (tUse[j]!=null) {
+						ok=false;
+					}
+				}
+				if (ok) {
+					for (int j = i; j < i+taille; j++) {
+						tUse[j]=true;
+					}
+					return i;
+				}
+			}
+		}
+		return -1;
 	}
 
 }
